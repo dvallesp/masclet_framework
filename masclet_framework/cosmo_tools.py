617b1a471dbc43b6adb3c876583475a7ed03ce2b
valles <vapeda@alumni.uv.es>
2020-03-21 17:46:00 +0100

    Add functions in the cosmo_tools module to compute critical and background densities.

--- masclet_framework/cosmo_tools.py
+++ masclet_framework/cosmo_tools.py
@@ -10,12 +10,13 @@ Provides useful tools to compute time from redshift, evolution of the critical d
 Created by David Vallés
 """
 
-#  Last update on 21/3/20 16:11
+#  Last update on 21/3/20 17:45
 
 import json
 import os
 import numpy as np
 from scipy import integrate
+from masclet_framework import units
 
 
 def write_cosmo_parameters(h, omega_m, omega_lambda, omega_b, filename='cosmo_parameters.json', path=''):
@@ -111,3 +112,43 @@ def LCDM_time(z1,z2,omega_m,omega_lambda,h):
         raise ValueError("Error greater than 1 per mil")
 
     return tH * t[0] / h
+
+
+def critical_density(h, z=0, omega_m=0, omega_lambda=0):
+    """
+    Computes the value of the critical density of the universe (for making k=0, Lambda=0) at a given redshift, z.
+    Units: solar masses per Mpc^3.
+
+    Args:
+        h: dimensionless Hubble parameter, H_0 = 100 h km/s/Mpc
+        z: redshift (don't specify if values at the present time required)
+        omega_m: matter density parameter, at z=0
+        omega_lambda: dark energy density parameter, at z=0
+
+    Returns:
+        Critical density, in M_\odot / Mpc^3
+    """
+    isu_0 = 3 * (100000 * h) ** 2 / (8*np.pi*units.G_isu)
+    if z == 0:
+        isu = isu_0
+    else:
+        isu = isu_0 * E(z, omega_m, omega_lambda) ** 2
+
+    return isu * units.kg_to_sun / units.m_to_mpc**3
+
+
+def background_density(h, omega_m, z=0):
+    """
+    Computes the value of the background (matter) density of the universe, at a given redshift z.
+    Units: solar masses per Mpc^3.
+
+    Args:
+        h: dimensionless Hubble parameter, H_0 = 100 h km/s/Mpc
+        z: redshift (don't specify if values at the present time required)
+        omega_m: matter density parameter, at z=0
+
+    Returns:
+        Bkg density, in M_\odot / Mpc^3
+    """
+    # rhoB = omega_m * rhoC0 / (1+z)^3
+    return omega_m * critical_density(h) * (1+z)**3
--- masclet_framework/units.py
+++ masclet_framework/units.py
@@ -10,7 +10,7 @@ Contains the basic units of the MASCLET code
 Created by David Vallés
 """
 
-#  Last update on 16/3/20 19:14
+#  Last update on 21/3/20 17:45
 
 ### FUNDAMENTAL UNITS; MASCLET TO PHYSICAL UNITS
 
@@ -18,7 +18,7 @@ mass_to_g = 1.816e52
 mass_to_kg = 1.816e49
 mass_to_sun = 9.1717e18
 
-length_to_cm = 3.38807679684064e+25
+length_to_cm = 3.3881e+25
 length_to_m = 3.3881e+23
 length_to_ly = 3.581e+7
 length_to_mpc = 10.98
@@ -27,6 +27,7 @@ time_to_s = 1.129e15
 time_to_yr = 35.8e6
 
 ### FUNDAMENTAL UNITS: PHYSICAL TO MASCLET UNITS
+
 g_to_mass = 1/mass_to_g
 kg_to_mass = 1/mass_to_kg
 sun_to_mass = 1/mass_to_sun
@@ -39,6 +40,25 @@ mpc_to_length = 1/length_to_mpc
 s_to_time = 1/time_to_s
 yr_to_time = 1/time_to_yr
 
+### OTHER USEFUL RELATIONS
+
+sun_to_kg = sun_to_mass * mass_to_kg
+sun_to_g = sun_to_mass * mass_to_g
+kg_to_sun = 1/sun_to_kg
+g_to_sun = 1/sun_to_g
+kg_to_g = 1000
+g_to_kg = 1/kg_to_g
+
+m_to_mpc = m_to_length * length_to_mpc
+mpc_to_m = 1/m_to_mpc
+cm_to_mpc = cm_to_length * length_to_mpc
+mpc_to_cm = 1/cm_to_mpc
+m_to_cm = 100
+cm_to_m = 1/m_to_cm
+
+yr_to_s = 3.154e7
+s_to_yr = 1/yr_to_s
+
 
 ### DERIVED UNITS OF COMMON USE: PHYSICAL TO MASCLET UNITS
 cgs_to_density = 2.1416e24
@@ -46,3 +66,9 @@ cgs_to_density = 2.1416e24
 
 ### DERIVED UNITS OF COMMON USE: MASCLET TO PHYSICAL UNITS
 density_to_cgs = 1/cgs_to_density
+
+
+### CONSTANTS
+G_isu = 6.67430e-11
+G_cgs = 6.67430e-8
+G_masclet = G_cgs * cm_to_length**3 / g_to_mass / s_to_time**2
