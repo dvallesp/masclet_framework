a384d7b0c172e0195fa438bd63d6c4b15658a0b7
valles <vapeda@alumni.uv.es>
2020-03-27 23:31:32 +0100

    Added function to compute r_\Delta using a particle list (instead of computing it from the grid)

--- masclet_framework/particles.py
+++ masclet_framework/particles.py
@@ -9,7 +9,7 @@ Contains several useful functions in order to deal with particles
 
 Created by David Vallés
 """
-#  Last update on 27/3/20 17:50
+#  Last update on 27/3/20 23:30
 
 # GENERAL PURPOSE AND SPECIFIC LIBRARIES USED IN THIS MODULE
 
@@ -21,6 +21,7 @@ from multiprocessing import Pool
 from masclet_framework import cosmo_tools, units
 
 import collections
+from scipy import optimize
 
 # FUNCTIONS DEFINED IN THIS MODULE
 
@@ -113,3 +114,47 @@ def shared_mass(x1, y1, z1, m1, oripa1, rx1, ry1, rz1, r1, x2, y2, z2, m2, oripa
     _, indices1, _ = np.intersect1d(inside_1, inside_2, return_indices=True, assume_unique=True)
 
     return m1[indices1].sum() * units.mass_to_sun
+
+
+def find_rDelta_eqn_particles(r, Delta, background_density, clusrx, clusry, clusrz, x, y, z, m, verbose):
+    if verbose:
+        print('Evaluating at r={:.3f}'.format(r))
+
+    inside_1 = (x - clusrx) ** 2 + (y - clusry) ** 2 + (z - clusrz) ** 2 < r ** 2
+    m = m[inside_1].sum()
+
+    return m - (4*np.pi/3) * r**3 * background_density * Delta
+
+
+def find_rDelta_particles(Delta, zeta, clusrx, clusry, clusrz, x, y, z, m, h, omega_m, rmin=0.1, rmax=6, rtol=1e-3,
+                          verbose=False):
+    """
+    Finds the value (in Mpc) of r_\Delta, the radius enclosing a mean overdensity (of the DM field, by default) equal
+    to Delta times the background density of the universe. By default, it uses the Brent method.
+
+    Args:
+        Delta: value of the desired overdensity
+        zeta: current redshift
+        clusrx, clusry, clusrz: comoving coordinates of the center of the sphere
+        x, y, z: vector with the particles' positions
+        m: vector with the particles' masses
+        h: dimensionless Hubble constant
+        omega_m: matter density parameter
+        rmin, rmax: starting two points (M(r) - Delta rho_B 4pi/3 r^3) must change sign
+        rtol: tolerance in the determination of r
+        verbose: if True, prints the patch being opened at a time
+
+    Returns:
+        The value of r_\Delta
+    """
+    background_density = cosmo_tools.background_density(h, omega_m, zeta)
+    args = (Delta, background_density, clusrx, clusry, clusrz, x, y, z, m, verbose)
+    try:
+        rDelta = optimize.brentq(find_rDelta_eqn_particles, rmin, rmax, args=args, xtol=rtol)
+    except ValueError:
+        print('Something might be wrong with this one... Might need further checking.')
+        return float('nan')
+    if verbose:
+        print('Converged!')
+    return rDelta
+
