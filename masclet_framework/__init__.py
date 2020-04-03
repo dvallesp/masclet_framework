88b110a7345770eabe529fcb4ae76eb8196f3776
valles <vapeda@alumni.uv.es>
2020-04-03 10:29:08 +0200

    Added tools_xyz module. It overlaps most of the features of the tools module, but will probably work quite faster.

--- masclet_framework/__init__.py
+++ masclet_framework/__init__.py
@@ -7,7 +7,7 @@ Also, serves as a bridge between MASCLET data and the yt package (v 3.5.1).
 Created by David Vallés
 """
 
-#  Last update on 27/3/20 9:48
+#  Last update on 3/4/20 10:29
 
 import masclet_framework.parameters
 import masclet_framework.read_masclet
@@ -18,3 +18,4 @@ import masclet_framework.units
 import masclet_framework.cosmo_tools
 import masclet_framework.read_asohf
 import masclet_framework.particles
+import masclet_framework.tools_xyz
