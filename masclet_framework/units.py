9e20e67f4ea5bda4b07562b89c9d5e325565387b
valles <vapeda@alumni.uv.es>
2022-06-15 12:34:06 +0200

    Add several more unit conversions and constants to the units.py module

--- masclet_framework/units.py
+++ masclet_framework/units.py
@@ -59,6 +59,10 @@ cm_to_mpc = 1/mpc_to_cm
 yr_to_s = 31556736
 s_to_yr = 1/yr_to_s
 
+eV_to_J = 1.6022e-19
+keV_to_J = 1e3 * eV_to_J
+J_to_eV = 1/eV_to_J
+J_to_keV = 1/keV_to_J
 
 ### DERIVED UNITS OF COMMON USE: PHYSICAL TO MASCLET UNITS
 cgs_to_density = 2.1416e24
@@ -72,6 +76,14 @@ density_to_cgs = 1/cgs_to_density
 G_isu = 6.67430e-11
 G_cgs = 6.67430e-8
 G_masclet = G_cgs * cm_to_length**3 / g_to_mass / s_to_time**2
+
+kB_isu = 1.380649e-23
+kB_cgs = 1.380649e-16
+#kB_masclet =
+
+mp_isu = 1.67262192e-27
+mp_cgs = mp_isu*1e3
+mp_masclet = mp_isu * kg_to_mass
 
 ### MAGNETIC FIELD
 magneticfield_to_gauss = 1/13.75
