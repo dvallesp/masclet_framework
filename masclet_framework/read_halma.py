7a2b95b1bdacb78815c9ec0cec16e8788c416c6a
oscarmonllor99 <oscar.monllor@uv.es>
2022-11-04 13:09:08 +0100

    Output of read_halma equivalent to read_asohf
    Output of read_halma equivalent to read_asohf, with a "legacy" option to return the catalogue information not as a list of dictionaries or dictionary of arrays (read_asohf), but as a list of 2D arrays (original HALMA reader).

--- masclet_framework/read_halma.py
+++ masclet_framework/read_halma.py
@@ -21,7 +21,8 @@ from cython_fortran_file import FortranFile as FF
 
 # FUNCTIONS DEFINED IN THIS MODULE
 
-def read_stellar_catalogue(path='', name='', old = False):
+def read_stellar_catalogue(path, name, old, legacy, it, 
+                    output_format, output_redshift, min_mass):
     """
     Reads halma_halo_stars_rp.res containing the stellar halo catalogue.
 
@@ -30,10 +31,21 @@ def read_stellar_catalogue(path='', name='', old = False):
         name: name of the catalogue file, typically "halma_halo_stars_rp.res"
         old: If old (bool), it assumes halma in 2017 version, which requires old_catalog_with_SFR_merger.npy file, 
             created with SFR_mergertype_old_catalog.py. Default is False, assuming halma27 (2022 version)
+        legacy: if legacy (bool), returns return1, else returns return2
 
-    Returns: num_iter: number of iterations (int)
-             total_halo_data[iteration][halo, data]: list of 2D-arrays  containing the halo data
+        (APLLIED ONLY IF legacy = False)
+        it: MASCLET iteration
+        output_format: 'dictionaries' or 'arrays
+        output_redshift: bool, if True, redshift is also returned
+        min_mass: minimum mass for a halo to be in the returned array or dictionary. If "None"
+                  it is skipped.
+
+    Return1: 
+             total_halo_data[iteration][halo, data]: list of 2D-arrays containing the halo data for every iteration
              total_iteration_data[iteration, data]: 2D-array containing the iteration data
+
+    Return2: 
+             list of dictionaries (one per halo) or dictionary of arrays, for iteration 'it'.
     """
     halma_catalogue = open(path+name, 'r')
 
@@ -81,14 +93,83 @@ def read_stellar_catalogue(path='', name='', old = False):
 
     halma_catalogue.close()
 
-    return num_iter, total_iteration_data, total_halo_data
+    ####################################
+    ####################################
+
+    if legacy:
+        return total_iteration_data, total_halo_data
+
+    else:
+        #finding it HALMA index
+        for it_halma in range(num_iter):
+            if total_iteration_data[it_halma, 3] == it:
+                break
+
+        zeta = total_iteration_data[it_halma, 5]
+        num_halos = int(total_iteration_data[it_halma, 0])
+        it_halo_data = total_halo_data[it_halma] 
+        haloes=[]
+        for ih in range(num_halos):
+            halo = {}
+            halo['id'] = it_halo_data[ih, 0]
+            halo['partNum'] = it_halo_data[ih, 1]
+            halo['M'] = it_halo_data[ih, 2]
+            halo['Mv'] = it_halo_data[ih, 3]
+            halo['Mgas'] = it_halo_data[ih, 4]
+            halo['fcold'] = it_halo_data[ih, 5]
+            halo['Mhotgas'] = it_halo_data[ih, 6]
+            halo['Mcoldgas'] = it_halo_data[ih, 7]
+            halo['Msfr'] = it_halo_data[ih, 8]
+            halo['Rmax'] = it_halo_data[ih, 9]
+            halo['R'] = it_halo_data[ih, 10]
+            halo['R_1d'] = it_halo_data[ih, 11]
+            halo['R_1dx'] = it_halo_data[ih, 12]
+            halo['R_1dy'] = it_halo_data[ih, 13]
+            halo['R_1dz'] = it_halo_data[ih, 14]
+            halo['sigma_v'] = it_halo_data[ih, 15]
+            halo['sigma_v_1d'] = it_halo_data[ih, 16]
+            halo['sigma_v_1dx'] = it_halo_data[ih, 17]
+            halo['sigma_v_1dy'] = it_halo_data[ih, 18]
+            halo['sigma_v_1dz'] = it_halo_data[ih, 19]
+            halo['L'] = it_halo_data[ih, 20]
+            halo['xcm'] = it_halo_data[ih, 21]
+            halo['ycm'] = it_halo_data[ih, 22]
+            halo['zcm'] = it_halo_data[ih, 23]
+            halo['vx'] = it_halo_data[ih, 24]
+            halo['vy'] = it_halo_data[ih, 25]
+            halo['vz'] = it_halo_data[ih, 26]
+            halo['father1'] = it_halo_data[ih, 27]
+            halo['father2'] = it_halo_data[ih, 28]
+            halo['nmerg'] = it_halo_data[ih, 29]
+            halo['mergType'] = it_halo_data[ih, 30]
+            halo['age_m'] = it_halo_data[ih, 31]
+            halo['age'] = it_halo_data[ih, 32]
+            halo['Z_m'] = it_halo_data[ih, 33]
+            halo['Z'] = it_halo_data[ih, 34]
+            haloes.append(halo)
+
+        if min_mass is not None:
+            haloes=[halo for halo in haloes if halo['M']>min_mass]
+
+        if output_format=='dictionaries':
+            if output_redshift:
+                return haloes, zeta
+            else:
+                return haloes
+        elif output_format=='arrays':
+            if output_redshift:
+                return {k: np.array([h[k] for h in haloes]) for k in haloes[0].keys()}, zeta
+            else:
+                return {k: np.array([h[k] for h in haloes]) for k in haloes[0].keys()}
+
+
 
 def read_halo_particles(it, path, total_halo_data, total_iteration_data, halo):
     """
     Reads the halma binary catalogue containing the information of every halo particle.
 
     Args:
-        it: iteration (iterations go from 0 to halma_final_iteration-1)
+        it: MASCLET ITERATION
         path: path of the binary catalogue (halotree)
         total_halo_data: list of 2D-arrays  containing the halo data
         total_iteration_Data: 2D-array containing the iteration data
@@ -99,14 +180,15 @@ def read_halo_particles(it, path, total_halo_data, total_iteration_data, halo):
     Returns: list of arrays of lenght the number of particles of the halo, containing the particle 
              information
     """
+    #finding it HALMA index
+    num_iter = len(total_halo_data)
+    for it_halma in range(num_iter):
+        if total_iteration_data[it_halma, 3] == it:
+            break
 
+    string_it = '{:05d}'.format(it)
 
-    it_masclet = int(total_iteration_data[it, 3])
-    string_it = str(it_masclet)
-    while len(string_it) < 5:
-        string_it = '0'+string_it
-
-    npart = int(total_halo_data[it][halo, 1])
+    npart = int(total_halo_data[it_halma][halo, 1])
     stpart_x = np.zeros((npart))
     stpart_y = np.zeros((npart))
     stpart_z = np.zeros((npart))
@@ -121,7 +203,7 @@ def read_halo_particles(it, path, total_halo_data, total_iteration_data, halo):
 
     low = 0
     for h in range(0, halo):
-        low += int(total_halo_data[it][h, 1])
+        low += int(total_halo_data[it_halma][h, 1])
 
     f_float = FF(path+'halotree'+string_it)
     f_int = FF(path+'halotree'+string_it)
