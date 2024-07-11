d24a3777c5dddbf56a887570a8930f0dbe4c1e28
valles <vapeda@alumni.uv.es>
2024-07-11 13:04:34 +0200

    Add Marco's 5-point stencil differential operators

--- masclet_framework/diff.py
+++ masclet_framework/diff.py
@@ -19,70 +19,159 @@ from masclet_framework import tools
 def arr_diff_x(arr):
     nx = arr.shape[0]
     difference = np.zeros_like(arr)
-    difference[1:nx-1,:,:] = (arr[2:nx,:,:] - arr[0:nx-2,:,:])
+    difference[1:nx-1,:,:] = arr[2:nx,:,:] - arr[0:nx-2,:,:]
     # Second order extrapolation at the boundary
     difference[0,:,:] = 4*arr[1,:,:] - 3*arr[0,:,:] - arr[2,:,:]
     difference[nx-1,:,:] = 3*arr[nx-1,:,:] + arr[nx-3,:,:] - 4*arr[nx-2,:,:]
     return difference
 
 @njit(fastmath=True)
+def arr_diff_x_5_stencil(arr):
+    '''
+    Computes the five-point stencil derivative in this coordinate direction.
+
+    Author: Marco Molina
+    '''
+    # The denominator is multiplied by 12*dx in the following functions
+    nx = arr.shape[0]
+    difference = np.zeros_like(arr)
+    difference[2:nx-2,:,:] = -arr[4:nx,:,:] + 8*arr[3:nx-1,:,:] - 8*arr[1:nx-3,:,:] + arr[0:nx-4,:,:] 
+    # Second order central difference at the second to last boundary
+    difference[1,:,:] = 6*(arr[2,:,:] - arr[0,:,:]) 
+    difference[nx-2,:,:] = 6*(arr[nx-1,:,:] - arr[nx-3,:,:]) 
+    # Second order extrapolation at the boundary
+    difference[0,:,:] = 6*(4*arr[1,:,:] - 3*arr[0,:,:] - arr[2,:,:]) 
+    difference[nx-1,:,:] = 6*(3*arr[nx-1,:,:] + arr[nx-3,:,:] - 4*arr[nx-2,:,:]) 
+    return difference
+
+@njit(fastmath=True)
 def arr_diff_y(arr):
     ny = arr.shape[1]
     difference = np.zeros_like(arr)
-    difference[:,1:ny-1,:] = (arr[:,2:ny,:] - arr[:,0:ny-2,:])
+    difference[:,1:ny-1,:] = arr[:,2:ny,:] - arr[:,0:ny-2,:]
     # Second order extrapolation at the boundary
     difference[:,0,:] = 4*arr[:,1,:] - 3*arr[:,0,:] - arr[:,2,:]
     difference[:,ny-1,:] = 3*arr[:,ny-1,:] + arr[:,ny-3,:] - 4*arr[:,ny-2,:]
     return difference
 
 @njit(fastmath=True)
+def arr_diff_y_5_stencil(arr):
+    '''
+    Computes the five-point stencil derivative in this coordinate direction.
+
+    Author: Marco Molina
+    '''
+    # The denominator is multiplied by 12*dx in the following functions
+    ny = arr.shape[1]
+    difference = np.zeros_like(arr)
+    difference[:,2:ny-2,:] = -arr[:,4:ny,:] + 8*arr[:,3:ny-1,:] - 8*arr[:,1:ny-3,:] + arr[:,0:ny-4,:] 
+    # Second order central difference at the second to last boundary
+    difference[:,1,:] = 6*(arr[:,2,:] - arr[:,0,:]) 
+    difference[:,ny-2,:] = 6*(arr[:,ny-1,:] - arr[:,ny-3,:]) 
+    # Second order extrapolation at the boundary
+    difference[:,0,:] = 6*(4*arr[:,1,:] - 3*arr[:,0,:] - arr[:,2,:]) 
+    difference[:,ny-1,:] = 6*(3*arr[:,ny-1,:] + arr[:,ny-3,:] - 4*arr[:,ny-2,:])
+    return difference
+
+@njit(fastmath=True)
 def arr_diff_z(arr):
     nz = arr.shape[2]
     difference = np.zeros_like(arr)
-    difference[:,:,1:nz-1] = (arr[:,:,2:nz] - arr[:,:,0:nz-2])
+    difference[:,:,1:nz-1] = arr[:,:,2:nz] - arr[:,:,0:nz-2]
     # Second order extrapolation at the boundary
     difference[:,:,0] = 4*arr[:,:,1] - 3*arr[:,:,0] - arr[:,:,2]
     difference[:,:,nz-1] = 3*arr[:,:,nz-1] + arr[:,:,nz-3] - 4*arr[:,:,nz-2]
     return difference
 
-@njit(fastmath=True)
-def arr_gradient(arr, dx):
-    den = np.float32(1/(2*dx))
-    return den*arr_diff_x(arr), den*arr_diff_y(arr), den*arr_diff_z(arr)
+    @njit(fastmath=True)
+def arr_diff_z_5_stencil(arr):
+    '''
+    Computes the five-point stencil derivative in this coordinate direction.
+
+    Author: Marco Molina
+    '''
+    # The denominator is multiplied by 12*dx in the following functions
+    nz = arr.shape[2]
+    difference = np.zeros_like(arr)
+    difference[:,:,2:nz-2] = -arr[:,:,4:nz] + 8*arr[:,:,3:nz-1] - 8*arr[:,:,1:nz-3] + arr[:,:,0:nz-4] 
+    # Second order central difference at the second to last boundary
+    difference[:,:,1] = 6*(arr[:,:,2] - arr[:,:,0]) 
+    difference[:,:,nz-2] = 6*(arr[:,:,nz-1] - arr[:,:,nz-3]) 
+    # Second order extrapolation at the boundary
+    difference[:,:,0] = 6*(4*arr[:,:,1] - 3*arr[:,:,0] - arr[:,:,2]) 
+    difference[:,:,nz-1] = 6*(3*arr[:,:,nz-1] + arr[:,:,nz-3] - 4*arr[:,:,nz-2]) 
+    return difference
 
 @njit(fastmath=True)
-def arr_gradient_magnitude(arr, dx):
-    den = np.float32(1/(2*dx))
-    return den*np.sqrt(arr_diff_x(arr)**2 + arr_diff_y(arr)**2 + arr_diff_z(arr)**2)
+def arr_gradient(arr, dx, stencil=3):
+    if stencil==3:
+        den = np.float32(1/(2*dx))
+        return den*arr_diff_x(arr), den*arr_diff_y(arr), den*arr_diff_z(arr)
+    elif stencil==5:
+        den = np.float32(1/(12*dx))
+        return den*arr_diff_x_5_stencil(arr), den*arr_diff_y_5_stencil(arr), den*arr_diff_z_5_stencil(arr)
 
 @njit(fastmath=True)
-def arr_divergence(arr_x, arr_y, arr_z, dx):
-    den = np.float32(1/(2*dx))
-    return (arr_diff_x(arr_x) + arr_diff_y(arr_y) + arr_diff_z(arr_z))*den
+def arr_gradient_magnitude(arr, dx, stencil=3):
+    if stencil == 3:
+        den = np.float32(1/(2*dx))
+        return den*np.sqrt(arr_diff_x(arr)**2 + arr_diff_y(arr)**2 + arr_diff_z(arr)**2)
+    elif stencil == 5:
+        den = np.float32(1/(12*dx))
+        return den*np.sqrt(arr_diff_x_5_stencil(arr)**2 + arr_diff_y_5_stencil(arr)**2 + arr_diff_z_5_stencil(arr)**2)
 
 @njit(fastmath=True)
-def arr_curl(arr_x, arr_y, arr_z, dx):
-    den = np.float32(1/(2*dx))
-    return den*(arr_diff_y(arr_z) - arr_diff_z(arr_y)), den*(arr_diff_z(arr_x) - arr_diff_x(arr_z)), den*(arr_diff_x(arr_y) - arr_diff_y(arr_x))
+def arr_divergence(arr_x, arr_y, arr_z, dx, stencil=3):
+    if stencil == 3:
+        den = np.float32(1/(2*dx))
+        return (arr_diff_x(arr_x) + arr_diff_y(arr_y) + arr_diff_z(arr_z))*den
+    elif stencil == 5:
+        den = np.float32(1/(12*dx))
+        return (arr_diff_x_5_stencil(arr_x) + arr_diff_y_5_stencil(arr_y) + arr_diff_z_5_stencil(arr_z))*den
 
 @njit(fastmath=True)
-def arr_curl_magnitude(arr_x, arr_y, arr_z, dx):
-    den = np.float32(1/(2*dx))
-    return den*np.sqrt((arr_diff_y(arr_z) - arr_diff_z(arr_y))**2 +
-                       (arr_diff_z(arr_x) - arr_diff_x(arr_z))**2 + 
-                       (arr_diff_x(arr_y) - arr_diff_y(arr_x))**2)
+def arr_curl(arr_x, arr_y, arr_z, dx, stencil=3):
+    if stencil == 3:
+        den = np.float32(1/(2*dx))
+        return den*(arr_diff_y(arr_z) - arr_diff_z(arr_y)), den*(arr_diff_z(arr_x) - arr_diff_x(arr_z)), den*(arr_diff_x(arr_y) - arr_diff_y(arr_x))
+    elif stencil == 5:
+        den = np.float32(1/(12*dx))
+        return den*(arr_diff_y_5_stencil(arr_z) - arr_diff_z_5_stencil(arr_y)), den*(arr_diff_z_5_stencil(arr_x) - arr_diff_x_5_stencil(arr_z)), den*(arr_diff_x_5_stencil(arr_y) - arr_diff_y_5_stencil(arr_x))
 
 @njit(fastmath=True)
-def arr_u_nabla_phi(arrphi, arru_x, arru_y, arru_z, dx):
-    den = np.float32(1/(2*dx))
-    return den*(arru_x*arr_diff_x(arrphi) + arru_y*arr_diff_y(arrphi) + arru_z*arr_diff_z(arrphi))
+def arr_curl_magnitude(arr_x, arr_y, arr_z, dx, stencil=3):
+    if stencil == 3:
+        den = np.float32(1/(2*dx))
+        return den*np.sqrt((arr_diff_y(arr_z) - arr_diff_z(arr_y))**2 +
+                        (arr_diff_z(arr_x) - arr_diff_x(arr_z))**2 + 
+                        (arr_diff_x(arr_y) - arr_diff_y(arr_x))**2)
+    elif stencil == 5:
+        den = np.float32(1/(12*dx))
+        return den*np.sqrt((arr_diff_y_5_stencil(arr_z) - arr_diff_z_5_stencil(arr_y))**2 +
+                        (arr_diff_z_5_stencil(arr_x) - arr_diff_x_5_stencil(arr_z))**2 + 
+                        (arr_diff_x_5_stencil(arr_y) - arr_diff_y_5_stencil(arr_x))**2)
 
 @njit(fastmath=True)
-def arr_u_nabla_v(arrv_x, arrv_y, arrv_z, arru_x, arru_y, arru_z, dx):
-    return arr_u_nabla_phi(arrv_x, arru_x, arru_y, arru_z, dx), arr_u_nabla_phi(arrv_y, arru_x, arru_y, arru_z, dx), arr_u_nabla_phi(arrv_z, arru_x, arru_y, arru_z, dx)
+def arr_u_nabla_phi(arrphi, arru_x, arru_y, arru_z, dx, stencil=3):
+    if stencil == 3:
+        den = np.float32(1/(2*dx))
+        return den*(arru_x*arr_diff_x(arrphi) + arru_y*arr_diff_y(arrphi) + arru_z*arr_diff_z(arrphi))
+    elif stencil == 5:
+        den = np.float32(1/(12*dx))
+        return den*(arru_x*arr_diff_x_5_stencil(arrphi) + arru_y*arr_diff_y_5_stencil(arrphi) + arru_z*arr_diff_z_5_stencil(arrphi))
+
+@njit(fastmath=True)
+def arr_u_nabla_v(arrv_x, arrv_y, arrv_z, arru_x, arru_y, arru_z, dx, stencil=3):
+    if stencil == 3:
+        den = np.float32(1/(2*dx))
+        return arr_u_nabla_phi(arrv_x, arru_x, arru_y, arru_z, dx), arr_u_nabla_phi(arrv_y, arru_x, arru_y, arru_z, dx), arr_u_nabla_phi(arrv_z, arru_x, arru_y, arru_z, dx)
+    elif stencil == 5:
+        den = np.float32(1/(12*dx))
+        return arr_u_nabla_phi(arrv_x, arru_x, arru_y, arru_z, dx, stencil=5), arr_u_nabla_phi(arrv_y, arru_x, arru_y, arru_z, dx, stencil=5), arr_u_nabla_phi(arrv_z, arru_x, arru_y, arru_z, dx, stencil=5)
+
 
 ## Fields stuff
-def gradient(field, dx, npatch, kept_patches=None):
+def gradient(field, dx, npatch, kept_patches=None, stencil=3):
     '''
     Computes the gradient of a scalar field defined on the AMR hierarchy of
      grids.
@@ -94,6 +183,7 @@ def gradient(field, dx, npatch, kept_patches=None):
         npatch: the number of patches in each direction
         kept_patches: 1d boolean array, True if the patch is kept, False if not.
                     If None, all patches are kept.
+        stencil: the stencil to use for the derivatives, either 3 or 5
 
     Returns:
         grad_x: a list of numpy arrays, each one containing the x-component of
@@ -115,7 +205,7 @@ def gradient(field, dx, npatch, kept_patches=None):
 
     for ipatch in prange(npatch.sum()+1):
         if kept_patches[ipatch]:
-            gx,gy,gz = arr_gradient(field[ipatch], resolution[ipatch])
+            gx,gy,gz = arr_gradient(field[ipatch], resolution[ipatch], stencil=stencil)
         else:
             gx,gy,gz = 0,0,0
         grad_x.append(gx)
@@ -124,7 +214,7 @@ def gradient(field, dx, npatch, kept_patches=None):
 
     return grad_x, grad_y, grad_z
 
-def divergence(field_x, field_y, field_z, dx, npatch, kept_patches=None):
+def divergence(field_x, field_y, field_z, dx, npatch, kept_patches=None, stencil=3):
     '''
     Computes the divergence of a vector field defined on the AMR hierarchy of
      grids.
@@ -139,6 +229,7 @@ def divergence(field_x, field_y, field_z, dx, npatch, kept_patches=None):
         npatch: the number of patches in each direction
         kept_patches: 1d boolean array, True if the patch is kept, False if not.
                     If None, all patches are kept.
+        stencil: the stencil to use for the derivatives, either 3 or 5
 
     Returns:
         div: a list of numpy arrays, each one containing the divergence of the
@@ -156,13 +247,13 @@ def divergence(field_x, field_y, field_z, dx, npatch, kept_patches=None):
 
     for ipatch in prange(npatch.sum()+1):
         if kept_patches[ipatch]:
-            div.append(arr_divergence(field_x[ipatch], field_y[ipatch], field_z[ipatch], resolution[ipatch]))
+            div.append(arr_divergence(field_x[ipatch], field_y[ipatch], field_z[ipatch], resolution[ipatch], stencil=stencil))
         else:
             div.append(0)
 
     return div
 
-def curl(field_x, field_y, field_z, dx, npatch, kept_patches=None):
+def curl(field_x, field_y, field_z, dx, npatch, kept_patches=None, stencil=3):
     '''
     Computes the curl of a vector field defined on the AMR hierarchy of
      grids.
@@ -177,6 +268,7 @@ def curl(field_x, field_y, field_z, dx, npatch, kept_patches=None):
         npatch: the number of patches in each direction
         kept_patches: 1d boolean array, True if the patch is kept, False if not.
                     If None, all patches are kept.
+        stencil: the stencil to use for the derivatives, either 3 or 5
 
     Returns:
         curl_x: a list of numpy arrays, each one containing the x-component of
@@ -198,7 +290,7 @@ def curl(field_x, field_y, field_z, dx, npatch, kept_patches=None):
 
     for ipatch in prange(npatch.sum()+1):
         if kept_patches[ipatch]:
-            cx,cy,cz = arr_curl(field_x[ipatch], field_y[ipatch], field_z[ipatch], resolution[ipatch])
+            cx,cy,cz = arr_curl(field_x[ipatch], field_y[ipatch], field_z[ipatch], resolution[ipatch], stencil=stencil)
         else:
             cx,cy,cz = 0,0,0
         curl_x.append(cx)
@@ -207,7 +299,7 @@ def curl(field_x, field_y, field_z, dx, npatch, kept_patches=None):
 
     return curl_x, curl_y, curl_z
 
-def curl_magnitude(field_x, field_y, field_z, dx, npatch, kept_patches=None):
+def curl_magnitude(field_x, field_y, field_z, dx, npatch, kept_patches=None, stencil=3):
     '''
     Computes the magnitude of the curl of a vector field defined on the 
      AMR hierarchy of grids.
@@ -222,6 +314,7 @@ def curl_magnitude(field_x, field_y, field_z, dx, npatch, kept_patches=None):
         npatch: the number of patches in each direction
         kept_patches: 1d boolean array, True if the patch is kept, False if not.
                     If None, all patches are kept.
+        stencil: the stencil to use for the derivatives, either 3 or 5
 
     Returns:
         curl_mag: a list of numpy arrays, each one containing the magnitude of
@@ -239,13 +332,13 @@ def curl_magnitude(field_x, field_y, field_z, dx, npatch, kept_patches=None):
 
     for ipatch in prange(npatch.sum()+1):
         if kept_patches[ipatch]:
-            curl_mag.append(arr_curl_magnitude(field_x[ipatch], field_y[ipatch], field_z[ipatch], resolution[ipatch]))
+            curl_mag.append(arr_curl_magnitude(field_x[ipatch], field_y[ipatch], field_z[ipatch], resolution[ipatch], stencil=stencil))
         else:
             curl_mag.append(0)
 
     return curl_mag
 
-def gradient_magnitude(field, dx, npatch, kept_patches=None):
+def gradient_magnitude(field, dx, npatch, kept_patches=None, stencil=3):
     '''
     Computes the magnitude of the gradient of a scalar field defined on 
      the AMR hierarchy of grids.
@@ -257,6 +350,7 @@ def gradient_magnitude(field, dx, npatch, kept_patches=None):
         npatch: the number of patches in each direction
         kept_patches: 1d boolean array, True if the patch is kept, False if not.
                     If None, all patches are kept.
+        stencil: the stencil to use for the derivatives, either 3 or 5
 
     Returns:
         grad_mag: a list of numpy arrays, each one containing the magnitude of
@@ -274,14 +368,14 @@ def gradient_magnitude(field, dx, npatch, kept_patches=None):
 
     for ipatch in prange(npatch.sum()+1):
         if kept_patches[ipatch]:
-            grad_mag.append(arr_gradient_magnitude(field[ipatch], resolution[ipatch]))
+            grad_mag.append(arr_gradient_magnitude(field[ipatch], resolution[ipatch], stencil=stencil))
         else:
             grad_mag.append(0)
 
     return grad_mag
 
 
-def directional_derivative_scalar_field(sfield, ufield_x, ufield_y, ufield_z, dx, npatch, kept_patches=None):
+def directional_derivative_scalar_field(sfield, ufield_x, ufield_y, ufield_z, dx, npatch, kept_patches=None, stencil=3):
     '''
     Computes (\vb{u} \cdot \nabla) \phi, where \vb{u} is a vector field and
         \phi is a scalar field, defined on the AMR hierarchy of grids.
@@ -298,6 +392,7 @@ def directional_derivative_scalar_field(sfield, ufield_x, ufield_y, ufield_z, dx
         npatch: the number of patches in each direction
         kept_patches: 1d boolean array, True if the patch is kept, False if not.
                     If None, all patches are kept.
+        stencil: the stencil to use for the derivatives, either 3 or 5
 
     Returns:
         u_nabla_phi: a list of numpy arrays, each one containing the result of
@@ -315,13 +410,13 @@ def directional_derivative_scalar_field(sfield, ufield_x, ufield_y, ufield_z, dx
 
     for ipatch in prange(npatch.sum()+1):
         if kept_patches[ipatch]:
-            u_nabla_phi.append(arr_u_nabla_phi(sfield[ipatch], ufield_x[ipatch], ufield_y[ipatch], ufield_z[ipatch], resolution[ipatch]))
+            u_nabla_phi.append(arr_u_nabla_phi(sfield[ipatch], ufield_x[ipatch], ufield_y[ipatch], ufield_z[ipatch], resolution[ipatch], stencil=stencil))
         else:
             u_nabla_phi.append(0)
 
     return u_nabla_phi
 
-def directional_derivative_vector_field(vfield_x, vfield_y, vfield_z, ufield_x, ufield_y, ufield_z, dx, npatch, kept_patches=None):
+def directional_derivative_vector_field(vfield_x, vfield_y, vfield_z, ufield_x, ufield_y, ufield_z, dx, npatch, kept_patches=None, stencil=3):
     '''
     Computes (\vb{u} \cdot \nabla) \vb{v}, where \vb{u} and \vb{v} are vector
         fields defined on the AMR hierarchy of grids.
@@ -341,6 +436,7 @@ def directional_derivative_vector_field(vfield_x, vfield_y, vfield_z, ufield_x, 
         npatch: the number of patches in each direction
         kept_patches: 1d boolean array, True if the patch is kept, False if not.
                     If None, all patches are kept.
+        stencil: the stencil to use for the derivatives, either 3 or 5
 
     Returns:
         u_nabla_v: a list of numpy arrays, each one containing the result of
@@ -360,7 +456,8 @@ def directional_derivative_vector_field(vfield_x, vfield_y, vfield_z, ufield_x, 
 
     for ipatch in prange(npatch.sum()+1):
         if kept_patches[ipatch]:
-            ux,uy,uz = arr_u_nabla_v(vfield_x[ipatch], vfield_y[ipatch], vfield_z[ipatch], ufield_x[ipatch], ufield_y[ipatch], ufield_z[ipatch], resolution[ipatch])
+            ux,uy,uz = arr_u_nabla_v(vfield_x[ipatch], vfield_y[ipatch], vfield_z[ipatch], ufield_x[ipatch], ufield_y[ipatch], ufield_z[ipatch], resolution[ipatch],
+                                        stencil=stencil)
         else:
             ux,uy,uz = 0,0,0
         u_nabla_v_x.append(ux)
