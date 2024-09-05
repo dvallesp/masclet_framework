4289f60213999b1296ee52e7a294eb90346d7ee4
valles <vapeda@alumni.uv.es>
2024-09-05 17:13:10 +0200

    Add functions to stats.py to compute correlations, partial correlations, non-parametric fits and my conditional non-parametric correlations

--- masclet_framework/stats.py
+++ masclet_framework/stats.py
@@ -14,6 +14,8 @@ Created by David Vallés
 
 import numpy as np
 import astropy.stats
+from scipy.stats import spearmanr
+from scipy.interpolate import UnivariateSpline
 
 def biweight_statistic(array, nmaxiter=100, tol=1e-4):
     '''
@@ -42,3 +44,122 @@ def biweight_statistic(array, nmaxiter=100, tol=1e-4):
     
     std = astropy.stats.biweight.biweight_scale(array, M=M, modify_sample_size=True)
     return M, std
+
+
+def correlation(x, y, mode='spearman'):
+    '''
+    Compute the correlation between x and y.
+
+    Note: 'pearson' correlation assumes linearity. 
+          'spearman' correlation assumes monotonicity.
+
+    Args:
+        x: first array (1d np.array)
+        y: second array (1d np.array)
+        mode: 'spearman' or 'pearson' (str)
+
+    Returns:
+        correlation: the correlation value (float)
+    '''
+    if mode == 'spearman':
+        return spearmanr(x, y).correlation
+    elif mode == 'pearson':
+        return np.corrcoef(x, y)[0,1]
+    else:
+        raise ValueError('Unknown mode')
+
+
+def partial_correlation(x, y, z, mode='spearman'):
+    ''' 
+    Compute the partial correlation between x and y given z, using the formula:
+
+    p_{xy.z} = (p_{xy} - p_{xz} p_{yz}) / sqrt((1-p_{xz}^2)(1-p_{yz}^2))
+
+    where p_{xy} is the correlation between x and y, and p_{xz} and p_{yz} are the correlations
+    between x and z, and y and z, respectively. This comes from fitting linear models x(z) and y(z)
+    and computing the correlation between the residuals.
+
+    The correlation can be computed in two modes: 'spearman' or 'pearson'.
+
+    Args:
+        x: first array (1d np.array)
+        y: second array (1d np.array)
+        z: third array (1d np.array)
+        mode: 'spearman' or 'pearson' (str)
+
+    Returns:
+        correlation: the partial correlation value (float)
+    '''
+    rho_xy = correlation(x, y, mode=mode)
+    rho_xz = correlation(x, z, mode=mode)
+    rho_yz = correlation(y, z, mode=mode)
+    return (rho_xy - rho_xz*rho_yz) / np.sqrt((1-rho_xz**2)*(1-rho_yz**2))
+
+
+def nonparametric_fit(x, y):
+    """ 
+    Fit a nonparametric smoothing spline to the data (x, y) using scipy's UnivariateSpline.
+    The smoothing parameter s is determined automatically, by estimating the standard deviation 
+    of the ordinates in a window around each point.
+
+    Args:
+        x: the x values (1d np.array)
+        y: the y values (1d np.array)
+
+    Returns:
+        interpolant: the interpolant function (UnivariateSpline)
+    """
+    # Order dots (requirement of scipy UnivariateSpline)
+    order = np.argsort(x)
+
+    # Determine the smoothing parameter s 
+    width = x.size//100
+    if width < 5:
+        width = 5
+    if width > 100:
+        width = 100
+    if width > x.size:
+        width = x.size
+
+    w = np.zeros_like(x)
+    for i in range(x.size):
+        i1 = max(0, i-width)
+        i2 = min(x.size, i+width)
+        w[i] = np.std(y[order][i1:i2])
+
+    w = 1/w
+    s = len(w)
+    w[:] = sum(w)/len(w)
+
+    interpolant = UnivariateSpline(x[order], y[order], k=3, s=s, w=w)
+    return interpolant
+
+def parametric_conditional_correlation(x, y, z, mode='spearman'):
+    """
+    Compute the conditional correlation between x and y given z, using a nonparametric fit
+    to estimate the residuals.
+
+    In detail, the function fits a nonparametric smoothing spline to the data (x, y) and computes
+    the residuals. Then, it computes the correlation between the residuals and z. The correlation
+    can be computed in two modes: 'spearman' or 'pearson'.
+
+    ****
+    Remark: unlike partial_correlation, this function does not assume linearity or monotonicity.
+    ****
+
+    Args:
+        x: first array (1d np.array)
+        y: second array (1d np.array)
+        z: third array (1d np.array)
+        mode: 'spearman' or 'pearson' (str)
+
+    Returns:
+        correlation: the conditional correlation value (float)
+    """
+    interpolant = nonparametric_fit(x, y)
+
+    # Compute the residuals
+    residuals = y - interpolant(x)
+
+    # Compute the correlation between the residuals and z
+    return correlation(residuals, z, mode=mode)
